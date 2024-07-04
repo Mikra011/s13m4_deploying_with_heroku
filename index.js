6b3f8665d4f955f9613fae1c6c6ab5ca52aa6a09
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-04 13:48:29 +0200

    Should be deployable

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const server = express();
 
@@ -10,6 +11,8 @@ server.get('/hello', (req, res) => {
 server.use('*', (req, res) => {
   res.json({ message: 'API is UP!' })
 });
+
+const port = process.env.PORT || 9000; // in case there is no PORT in either process.env nor the environment
 
 server.listen(9000, () => {
   console.log("\n*** Server Running on http://localhost:9000 ***\n");
